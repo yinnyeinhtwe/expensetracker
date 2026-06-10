0e13c37d2fcd8621f1190c3d5cd6f9b5387069a9
yinnyeinhtwe <kangjiinee@gmail.com>
2026-06-10 10:08:26 +0630

    Add expense

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,18 +1,22 @@
 import './App.css';
 import './index.css';
 import { ExpenseList } from './components/ExpenseList';
+import { useSelector } from 'react-redux';
 // import { CounterList } from './components/CounterList';
 // import { MovieInput } from './components/MovieInput';
 // import { MovieList } from './components/MovieList'; 
 
 function App() {
-
+  const expenses = useSelector((state) => state.expense.expenses);
   return (
     <>
       {/* <CounterList /> */}
       {/* <MovieInput />
       <MovieList /> */}
-      <ExpenseList />
+      <div>
+            {/* Pass the extracted array to your list component */}
+            <ExpenseList expenses={expenses} />
+        </div>
     </>
   );
 }
--- redux/src/slices/expenseSlice.js
+++ redux/src/slices/expenseSlice.js
@@ -6,6 +6,7 @@ const expenseSlice = createSlice({
     name: "expense",
     initialState: {
         expenses: saveExpenses? JSON.parse(saveExpenses): [{id: 1, description: "Salary", amount: 1000, type: "income"}, {id: 2, description: "Rent", amount: 500, type: "expense"}],
+        // totalIncome: 0,
     },
     reducers: {
         addExpense: (state, action) => {
@@ -16,9 +17,35 @@ const expenseSlice = createSlice({
                 type: action.payload.type,
             };
             state.expenses.push(newExpense);
-        }
-    },
+        },
+
+        deleteExpense: (state, action) => {
+            console.log("The payload is:", action.payload);
+            state.expenses = state.expenses.filter((expense) => expense.id !== action.payload);
+        },
+
+        editExpense: (state, action) => {
+            const {id, description, amount, type} = action.payload;
+            const existingExpense = state.expenses.find((expense) => expense.id === id);
+            if (existingExpense) {
+                existingExpense.description = description;
+                existingExpense.amount = amount;
+                existingExpense.type = type;
+            }
+        },
+
+        // calculateTotalIncome: (state) => {
+        //     const income = state.expenses.filter((expense) => expense.type === "income");
+        //     let incomeTotal=0;
+        //     income.forEach(item => {
+        //         incomeTotal += item.amount;
+        //     });
+        //     state.totalIncome = incomeTotal;
+        //     // return totalIncome;
+        //  },
+
+    }
 });
 
-export const { addExpense } = expenseSlice.actions;
+export const { addExpense, deleteExpense, editExpense, calculateTotalIncome } = expenseSlice.actions;
 export default expenseSlice.reducer;
